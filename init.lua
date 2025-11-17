435b71f89c5526371613affbe10692d4507f92fd
Logan Lang <devloglogan@gmail.com>
2025-11-17 08:09:50 -0600

    Add obsidian nvim plugin

--- init.lua
+++ init.lua
@@ -44,8 +44,8 @@ vim.opt.showmode = false -- Don't show mode in command line
 vim.opt.pumheight = 10 -- Popup menu height
 vim.opt.pumblend = 10 -- Popup menu transparency
 vim.opt.winblend = 0 -- Floating window transparency
-vim.opt.conceallevel = 0 -- Don't hide markup
-vim.opt.concealcursor = '' -- Show markup even on cursor line
+vim.opt.conceallevel = 1 -- Allow concealing (for markdown mostly)
+vim.opt.concealcursor = 'n' -- Allow concealing on cursor line in normal mode
 vim.opt.lazyredraw = false -- redraw while executing macros (butter UX)
 vim.opt.redrawtime = 10000 -- Timeout for syntax highlighting redraw
 vim.opt.maxmempattern = 20000 -- Max memory for pattern matching
@@ -257,6 +257,29 @@ require('lazy').setup({
         end, { desc = 'Open Mini Files' }),
       }
     end,
+  },
+
+  -- TODO: Might require nvim cmp for autocomplete
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*',
+    lazy = true,
+    ft = 'markdown',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      workspaces = {
+        {
+          name = 'notes',
+          path = '~/notes',
+        },
+      },
+      ui = {
+        checkboxes = {
+          [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+          ['x'] = { char = '', hl_group = 'ObsidianDone' },
+        },
+      },
+    },
   },
 
   { -- Useful plugin to show you pending keybinds.
