02d5087a68d1690acfd8d020e013df7d507f23d9
Logan Lang <devloglogan@gmail.com>
2025-12-24 15:17:15 -0600

    Add fix tab snipped issue

--- init.lua
+++ init.lua
@@ -177,6 +177,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+function leave_snippet()
+  if
+    ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+    and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+    and not require('luasnip').session.jump_active
+  then
+    require('luasnip').unlink_current()
+  end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command [[
+    autocmd ModeChanged * lua leave_snippet()
+]]
+
 local gdproject = io.open(vim.fn.getcwd() .. '/project.godot', 'r')
 if gdproject then
   io.close(gdproject)
